2dfc5b9b0e3cee0d8dbb80fdd49601925e0c2b5c
Lucas Wilhelm <lwilhelm@java.nas.local>
2013-11-18 10:07:14 -0500

    Added new java files

---
+++ chapter6/Fibonacci.java
@@ -0,0 +1,12 @@
+class Fibonacci {
+	public static void main(String[] args) {
+		System.out.println(fibonacci(8));
+}
+	public static void int fibonacci(int n) {
+	  if(n == 0 || n == 1) {
+	     return 1;
+	  } else {
+	     return fibonacci(n-1) + fibonacci(n-2);
+     }
+   }
+ }
